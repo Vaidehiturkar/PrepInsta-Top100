efc3b2da8b34060f4e345dded6efec74bbc31595
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-21 22:49:45 +0530

    Q01 Brute Force Solution

--- Basics/Q01_PositiveOrNegative.java
+++ Basics/Q01_PositiveOrNegative.java
@@ -1,5 +1,19 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class Q01_PositiveOrNegative {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n > 0)
+            System.out.println("Positive");
+        else if(n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
 }
