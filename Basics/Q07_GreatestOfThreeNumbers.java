3d44950ea513d80e7d113c300b42c6b38258a683
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-22 23:00:31 +0530

    Q07 Bruteforce Solution

---
+++ Basics/Q07_GreatestOfThreeNumbers.java
@@ -0,0 +1,30 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q07_GreatestOfThreeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("Greatest Number = " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("Greatest Number = " + num2);
+        } else {
+            System.out.println("Greatest Number = " + num3);
+        }
+
+        sc.close();
+    }
+}
