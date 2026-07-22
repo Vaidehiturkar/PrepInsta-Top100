fb2ced321c89e182bf44564fbabf57f66bbfc30a
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-22 23:02:29 +0530

    Q07 Optimal Solution

---
+++ Basics/Q07_GreatestOfThreeNumbers.java
@@ -0,0 +1,28 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q07_GreatestOfThreeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        int greatest = (num1 > num2)
+                ? ((num1 > num3) ? num1 : num3)
+                : ((num2 > num3) ? num2 : num3);
+
+        System.out.println("Greatest Number = " + greatest);
+
+        sc.close();
+    }
+}
