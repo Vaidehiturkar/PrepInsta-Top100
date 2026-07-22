4bd451eb04d26c8ee129b04057aee3f045a4ee66
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-22 23:51:11 +0530

    Q08 Optimal Solution

---
+++ Basics/Q08_LeapYear.java
@@ -0,0 +1,22 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q08_LeapYear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the year: ");
+        int year = sc.nextInt();
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+
+        sc.close();
+    }
+}
