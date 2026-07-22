5646a47d02d3aeebed86e17b17c2f2306c6d46fa
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-22 20:12:19 +0530

    Add Q03 Optimal Solution

---
+++ Basics/Q03_SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q03_SumOfFirstNNaturalNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = n * (n + 1) / 2;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
