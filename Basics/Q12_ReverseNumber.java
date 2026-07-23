f46e4c4418c84a2695f1cb5a3d772a767b816cbb
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-23 20:36:12 +0530

    Q12 Optimal Solution

---
+++ Basics/Q12_ReverseNumber.java
@@ -0,0 +1,25 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q12_ReverseNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int reverse = 0;
+
+        while (n > 0) {
+            reverse = reverse * 10 + (n % 10);
+            n /= 10;
+        }
+
+        System.out.println("Reverse number = " + reverse);
+
+        sc.close();
+    }
+}
