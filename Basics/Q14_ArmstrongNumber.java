e2c1bc8c7e5e170ec2e76c6d95cacc05174d0f86
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-23 21:43:59 +0530

    Q14 Optimal Solution

---
+++ Basics/Q14_ArmstrongNumber.java
@@ -0,0 +1,42 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q14_ArmstrongNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int temp = num;
+        int digits = 0;
+        int sum = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp = temp / 10;
+        }
+
+        temp = num;
+
+        while (temp != 0) {
+
+            int rem = temp % 10;
+
+            sum = sum + (int) Math.pow(rem, digits);
+
+            temp = temp / 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+
+        sc.close();
+    }
+}
