c8ae8e5c82c4505139627e940eb6892a22742557
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-25 23:25:13 +0530

    Q16 Optimal Solution

---
+++ Basics/Q16_FibonacciSeries.java
@@ -0,0 +1,28 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q16_FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        sc.close();
+    }
+}
