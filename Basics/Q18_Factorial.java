4db2acb654de59704ddc4b1e6de50bf491dec5f2
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-27 22:05:36 +0530

    Q18 Bruteforce Solution

---
+++ Basics/Q18_Factorial.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q18_Factorial {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+
+        sc.close();
+    }
+}
