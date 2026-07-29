a22198c2198830e1ce46c9045b2513f7236495d6
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-29 18:15:29 +0530

    Q20 Bruteforce Solution

---
+++ Basics/Q20_FactorsOfNumber.java
@@ -0,0 +1,25 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Q20_FactorsOfNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Factors are: ");
+
+        for (int i = 1; i < num; i++) {
+
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
