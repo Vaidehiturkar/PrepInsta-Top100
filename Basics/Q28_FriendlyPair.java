29f9525f57e04cb6d74962fd7715f6d0023bea14
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-07-30 22:19:18 +0530

    Q28 Optimal Solution

--- Basics/Q28_FriendlyPair.java
+++ Basics/Q28_FriendlyPair.java
@@ -14,20 +14,8 @@ public class Q28_FriendlyPair {
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
 
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (int i = 1; i < num1; i++) {
-            if (num1 % i == 0) {
-                sum1 = sum1 + i;
-            }
-        }
-
-        for (int i = 1; i < num2; i++) {
-            if (num2 % i == 0) {
-                sum2 = sum2 + i;
-            }
-        }
+        int sum1 = getSum(num1);
+        int sum2 = getSum(num2);
 
         if ((double) sum1 / num1 == (double) sum2 / num2)
             System.out.println("Friendly Pair");
@@ -35,5 +23,26 @@ public class Q28_FriendlyPair {
             System.out.println("Not a Friendly Pair");
 
         sc.close();
+    }
+
+    static int getSum(int num) {
+
+        if (num == 1)
+            return 0;
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= num; i++) {
+
+            if (num % i == 0) {
+
+                sum = sum + i;
+
+                if (i != num / i)
+                    sum = sum + (num / i);
+            }
+        }
+
+        return sum;
     }
 }
