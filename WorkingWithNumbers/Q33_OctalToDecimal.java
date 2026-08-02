c4fec87c4eb2294cbd3ea20d1b57e4739db529a6
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-08-02 21:10:34 +0530

    Q33 Bruteforce Solution

---
+++ WorkingWithNumbers/Q33_OctalToDecimal.java
@@ -0,0 +1,31 @@
+package WorkingWithNumbers;
+
+import java.util.Scanner;
+
+public class Q33_OctalToDecimal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an octal number: ");
+        int octal = sc.nextInt();
+
+        int decimal = 0;
+        int power = 0;
+
+        while (octal > 0) {
+
+            int rem = octal % 10;
+
+            decimal = decimal + (rem * (int) Math.pow(8, power));
+
+            power++;
+            octal = octal / 10;
+        }
+
+        System.out.println("Decimal = " + decimal);
+
+        sc.close();
+    }
+}
