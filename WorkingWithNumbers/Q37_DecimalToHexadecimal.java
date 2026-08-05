c0b5457c3d258bbdd96bd75a78bf74b9815963a2
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-08-05 20:33:26 +0530

    Q37 Optimal Solution

---
+++ WorkingWithNumbers/Q37_DecimalToHexadecimal.java
@@ -0,0 +1,43 @@
+package WorkingWithNumbers;
+
+import java.util.Scanner;
+
+public class Q37_DecimalToHexadecimal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        char[] digits = {
+                '0', '1', '2', '3',
+                '4', '5', '6', '7',
+                '8', '9', 'A', 'B',
+                'C', 'D', 'E', 'F'
+        };
+
+        char[] hex = new char[20];
+        int i = 0;
+
+        while (decimal > 0) {
+
+            int rem = decimal % 16;
+
+            hex[i] = digits[rem];
+
+            i++;
+
+            decimal = decimal / 16;
+        }
+
+        System.out.print("Hexadecimal number: ");
+
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(hex[j]);
+        }
+
+        sc.close();
+    }
+}
