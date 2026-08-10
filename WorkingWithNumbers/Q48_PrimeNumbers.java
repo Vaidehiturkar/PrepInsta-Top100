5d9cc8d31e8d747e401805d260448e3ede0e5d0d
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-08-10 20:55:16 +0530

    Q48 Bruteforce Solution

---
+++ WorkingWithNumbers/Q48_PrimeNumbers.java
@@ -0,0 +1,25 @@
+package WorkingWithNumbers;
+
+public class Q48_PrimeNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Prime numbers between 1 and 100:");
+
+        for (int num = 2; num <= 100; num++) {
+
+            boolean prime = true;
+
+            for (int i = 2; i < num; i++) {
+
+                if (num % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+                System.out.print(num + " ");
+        }
+    }
+}
