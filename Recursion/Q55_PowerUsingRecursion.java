915b9e0a0ff1ba4bcfd349409dec1356fc154d3e
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-08-12 19:43:43 +0530

    Q55 Optimal SOlution

---
+++ Recursion/Q55_PowerUsingRecursion.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Q55_PowerUsingRecursion {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter power: ");
+        int power = sc.nextInt();
+
+        int result = 1;
+
+        for (int i = 1; i <= power; i++) {
+            result = result * base;
+        }
+
+        System.out.println("Result = " + result);
+
+        sc.close();
+    }
+}
