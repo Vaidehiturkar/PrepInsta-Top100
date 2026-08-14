a40fc761fa8b8178fd2a409af04ccbfb5165c7bf
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-08-14 23:24:10 +0530

    Q59 Bruteforce Solution

---
+++ Recursion/Q59_ReverseNumber.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Q59_ReverseNumber {
+
+    static int reverse(int num, int rev) {
+
+        if (num == 0)
+            return rev;
+
+        int rem = num % 10;
+
+        rev = rev * 10 + rem;
+
+        return reverse(num / 10, rev);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = reverse(num, 0);
+
+        System.out.println("Reverse = " + result);
+
+        sc.close();
+    }
+}
