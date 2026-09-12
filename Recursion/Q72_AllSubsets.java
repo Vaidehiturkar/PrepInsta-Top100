600d1cb1b65df277a37fcd379b156d29bc1fe015
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-09-12 20:53:08 +0530

    Q72 Optimal Solution

---
+++ Recursion/Q72_AllSubsets.java
@@ -0,0 +1,37 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Q72_AllSubsets {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < (1 << n); i++) {
+
+            System.out.print("{ ");
+
+            for (int j = 0; j < n; j++) {
+
+                if ((i & (1 << j)) != 0) {
+                    System.out.print(arr[j] + " ");
+                }
+            }
+
+            System.out.println("}");
+        }
+
+        sc.close();
+    }
+}
