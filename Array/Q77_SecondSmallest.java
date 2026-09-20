8d4191cc1252d1ecd6176b61498aa2e683cdf110
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-09-20 22:18:36 +0530

    Q77 Bruteforce Solution

---
+++ Array/Q77_SecondSmallest.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Q77_SecondSmallest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println("Second Smallest = " + arr[1]);
+
+        sc.close();
+    }
+}
