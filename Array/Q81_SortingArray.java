f6a72acdc7e155fec9aac2b00b14c43c745645d9
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-09-24 21:58:26 +0530

    Q81 Bruteforce Solition

---
+++ Array/Q81_SortingArray.java
@@ -0,0 +1,42 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Q81_SortingArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
