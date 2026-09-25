34e4180bde19b4b52d1f3816d4a219f03ce4247f
Vaidehiturkar <vaidehiturkar22@gmail.com>
2026-09-25 23:05:28 +0530

    Q82 Optimal Solution

---
+++ Array/Q82_FrequencyOfElements.java
@@ -0,0 +1,40 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Q82_FrequencyOfElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (int num : map.keySet()) {
+            System.out.println(num + " = " + map.get(num));
+        }
+
+        sc.close();
+    }
+}
